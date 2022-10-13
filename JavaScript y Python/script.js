6dbd25fe1301c9f571dfdede9313f4f5c0c019af
Juan Esteban Atehortúa González <106275244+JuanesAth@users.noreply.github.com>
2022-10-13 16:54:00 -0500

    Add files via upload

--- JavaScript y Python/script.js
+++ JavaScript y Python/script.js
@@ -207,3 +207,133 @@
 
         NOTA: Si el número no se puede convertir, " NaN " se devuelve (No es un número).
         ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+        //SEGUNDA PARTE
+
+        /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        new Date()                                                              #Hora y fecha actuales.
+        new Date(year, month, day, hours, minutes, seconds, milliseconds)       #Asignar fecha.
+
+        Nota: JavaScript cuenta los meses del 0 al 11.
+        Especificar un mes superior a 11 no generará un error, pero agregará el desbordamiento al año siguiente
+
+        new Date(milliseconds)  JavaScript almacena fechas como milisegundos
+        Nota: La hora cero es el 1 de enero de 1970 a las 00:00:00 UTC.
+
+        new Date(date string)
+
+        Metodos:    .toString()
+                    .toUTCString()
+                    .toDateString()  ---->Mas legible
+                    .toISOString()
+
+                    .getFullYear()      Devuelve año de una fecha como un numero de 4 digitos
+                    .getMonth()         Devuelve mes como un numero (0-11)
+                    .getDate()          Devuelve el dia de una fecha (1-31)
+                    .getHours()         Devuelve las horas de una fecha (0-23)
+                    .getMinutes()       (0-59)
+                    .getSeconds()       (0-59)
+                    .getMilliseconds()  (0-999)
+                    .getTime()           Devuelve el número de milisegundos desde el 1 de enero de 1970
+
+        ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        function fechas() {
+            {
+            document.getElementById("titulo").innerHTML = "Fechas";
+            }
+           
+            const a = new Date();
+            document.getElementById("demo").innerHTML = a;
+
+            const b = new Date(2018, 15, 24, 10, 33, 30);
+            document.getElementById("demo2").innerHTML = b;
+
+            const c = new Date(0);
+            document.getElementById("demo3").innerHTML = c;
+            const d = new Date(100000000000);                   //100.000.000.000 milisegundos
+            document.getElementById("demo4").innerHTML = d;
+
+            const e = new Date("October 13, 2014 11:13:00");
+            document.getElementById("demo5").innerHTML = e;
+        }
+
+        /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        new Date()                                                              #Hora y fecha actuales.
+        new Date(year, month, day, hours, minutes, seconds, milliseconds)       #Asignar fecha.
+
+        Nota: JavaScript cuenta los meses del 0 al 11.
+        Especificar un mes superior a 11 no generará un error, pero agregará el desbordamiento al año siguiente
+
+        new Date(milliseconds)  JavaScript almacena fechas como milisegundos
+        Nota: La hora cero es el 1 de enero de 1970 a las 00:00:00 UTC.
+
+        new Date(date string)
+
+        Metodos:    .toString()
+                    .toUTCString()
+                    .toDateString()  ---->Mas legible
+                    .toISOString()
+
+                    .getFullYear()      Devuelve año de una fecha como un numero de 4 digitos
+                    .getMonth()         Devuelve mes como un numero (0-11)
+                    .getDate()          Devuelve el dia de una fecha (1-31)
+                    .getHours()         Devuelve las horas de una fecha (0-23)
+                    .getMinutes()       (0-59)
+                    .getSeconds()       (0-59)
+                    .getMilliseconds()  (0-999)
+                    .getTime()           Devuelve el número de milisegundos desde el 1 de enero de 1970
+
+                    .setDate()          (1-31)
+                    .setFullYear()      (Año) o (año, mes ,día)
+                    .setHours()         (0-23)
+                    .setMilliseconds()  (0-999)
+                    .setMinutes()       (0-59)
+                    .setMonth()         (0-11)
+                    .setSeconds()       (0-59)
+                    .setTime()          (Asignar fecha en milisegundos)
+
+
+        ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        function fechas() {
+            {
+            document.getElementById("titulo").innerHTML = "Fechas";
+            }
+           
+            const a = new Date();
+            document.getElementById("demo").innerHTML = a;
+
+            const b = new Date(2018, 15, 24, 10, 33, 30);
+            document.getElementById("demo2").innerHTML = b;
+
+            const c = new Date(0);
+            document.getElementById("demo3").innerHTML = c;
+            const d = new Date(100000000000);                   //100.000.000.000 milisegundos
+            document.getElementById("demo4").innerHTML = d;
+
+            const e = new Date("October 13, 2014 11:13:00");
+            document.getElementById("demo5").innerHTML = e;
+        }
+
+        /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        Math.round(x)       Devuelve x redondeado a su entero más cercano  
+        Math.ceil(x)        Devuelve x redondeado a su proximo numero
+        Math.floor(x)       Devuelve x redondeado hacia abajo
+        Math.trunc(x)       Devulve la parte entera
+        Math.sign(x)        Devulve el signo
+
+        Math.pow(x, y)      Potencia
+        Math.sqrt(x)        Raiz
+        Math.abs(x)         Valor absoluto
+        Matemáticas.min() y Matemáticas.max()       El valor mayor o menos de una lista de argumentos
+
+        Math.random();
+        ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+        function random() {
+            {
+            document.getElementById("titulo").innerHTML = "Matematicas";
+            }
+            a = Math.floor(Math.random() * (0 - 100)) + 100;
+            document.getElementById("demo5").innerHTML = a;
+        }
+
